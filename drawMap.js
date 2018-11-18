4d4edb378db5b17996a58ada220d21ce38c8a3d4
mtjvankuik <mtjvankuik@gmail.com>
2018-11-18 11:41:40 +0100

    Moved drawing code to seperate js file and imported drawMap.js in index.html

---
+++ drawMap.js
@@ -0,0 +1,51 @@
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'data/cities.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(JSON.parse(xobj.responseText));
+        }
+    };
+    xobj.send(null);
+}
+
+loadJSON(function(array) {
+
+    console.log(array); // this will log out the json object
+    google.charts.load('current', {
+        'packages': ['geochart'],
+        // Note: you will need to get a mapsApiKey for your project.
+        // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+        'mapsApiKey': 'AIzaSyA9RNP4ksFWb5Du48Ngghae8Jhg8Ia3oKY'
+    });
+    google.charts.setOnLoadCallback(drawRegionsMap);
+
+    function drawRegionsMap() {
+        //create new data table
+        var data = new google.visualization.DataTable();
+        //console.log(array);
+        //add columns for city and flu count
+        data.addColumn('string', 'City');
+        data.addColumn('number', 'Flu count');
+
+        //add the values to the columns
+        for (let i = 0; i < array.length; i++) {
+            data.addRows([[array[i].location, array[i].count]]);
+        }
+
+        //change the default settings in map
+        var options = {
+            region: 'GB',
+            resolution: 'provinces',
+            displayMode: 'markers',
+            markerOpacity: 0.8,
+            sizeAxis: {minSize:0,  maxSize: 10},
+            colorAxis: {colors: ['#b6e757', '#e05923']}
+        };
+
+        //create chart and put the chart in a div in the HTML
+        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+        chart.draw(data, options);
+    }
+});
